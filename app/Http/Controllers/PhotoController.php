1765b4f435e86f350702f264e8b5150314b8bc84
Ismy Amnestya Yunani <ismyamnes@gmail.com>
2019-12-08 13:46:01 +0700

    add photo model and migration photos.index from database

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Photo;
 use Illuminate\Http\Request;
 
 class PhotoController extends Controller
 {
     public function index()
     {
-        return view('photos.index');
+        $photos = Photo::all();
+
+        return view('photos.index', ['photos' => $photos]);
     }
 
     public function show($id)
